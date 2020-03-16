12a382f2b0990798454702df4090aab301356fde
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-03-16 16:01:41 -0700

    Moved state to App component

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,32 @@ import React from 'react';
 import QueryForm from "./QueryForm";
 
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            searchParam: 'null'
+        }
+    }
+    
+    handleSearch = (e) => {
+        e.preventDefault();
+        console.log(this.state.searchParam);
+    }
+
+    handleClear = (e) => {
+        document.querySelector('.query-form').reset();
+    }
+
+    handleChange = (e) => {
+        this.setState({searchParam : e.target.value});
+    }
 
     render() {
         return (
             <div>
                 <h1>Book Finder</h1>
                 <br/>
-                <QueryForm/>
+                <QueryForm handleSearch={this.handleSearch} handleClear={this.handleClear} handleChange={this.handleChange}/>
             </div>
         );
     };
--- src/components/QueryForm.js
+++ src/components/QueryForm.js
@@ -2,33 +2,13 @@ import React from 'react';
 
 class QueryForm extends React.Component {
 
-    constructor() {
-        super();
-        this.state = {
-            searchParam: 'null'
-        }
-    }
-    
-    handleSearch = (e) => {
-        e.preventDefault();
-        //Make API call
-    }
-
-    handleClear = (e) => {
-        document.querySelector('.query-form').reset();
-    }
-
-    handleChange = (e) => {
-        this.setState({searchParam : e.target.value});
-    }
-
     render() {
         return (
             <div>
-                <form className="query-form" onSubmit={this.handleSearch}>
+                <form className="query-form" onSubmit={this.props.handleSearch}>
                     <label> Enter Search Query:
-                        <input type="text" onChange={this.handleChange}placeholder="title/author/subject"/>
-                        <input readOnly type="text" value="X" onClick={this.handleClear}/>
+                        <input type="text" onChange={this.props.handleChange}placeholder="title/author/subject"/>
+                        <input readOnly type="text" value="X" onClick={this.props.handleClear}/>
                        
                     </label>
                     <input type="submit" value="Submit"/>
